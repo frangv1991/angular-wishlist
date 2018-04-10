0d27fbebd6883c0c82ddd4d13e94d0d34b06f0f7
Francisco Gonzalez Valle <fgonzalezv@ayesa.es>
2018-04-10 18:10:59 +0200

    Two objects

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -7,13 +7,12 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 2, title: 'Spyro', onSale: false, platform: 'PS4' },
       { id: 3, title: 'Xenoblade Chronicles 2', onSale: true, platform: 'Switch' }
     ];
-/*
+
     const platforms = [
       { id: 1, name: 'PS4', actualGen: true, videogames: ['Crash Bandicoot', 'Spyro'] },
       { id: 2, name: 'Switch', actualGen: true, videogames: ['Xenoblade Chronicles 2'] }
     ];
-*/
 
-    return {videogames};
+    return {videogames, platforms};
   }
 }
